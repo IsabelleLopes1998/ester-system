d494cefc8d3ab33c669ac6b57fdf77b1ade90cda
Wicar Pessoa <wicarpp@gmail.com>
2025-04-14 17:53:41 -0300

    feat: create auths dtos

--- src/main/java/com/br/demo/dto/request/AuthRequestDTO.java
+++ src/main/java/com/br/demo/dto/request/AuthRequestDTO.java
@@ -1,4 +1,14 @@
 package com.br.demo.dto.request;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthRequestDTO {
+    private String username;
+    private String senha;
 }
