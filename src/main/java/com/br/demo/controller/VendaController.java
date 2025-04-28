f2fadd6f25c0440ae3388b27aee95a1350a729fb
Wicar Pessoa <wicarpp@gmail.com>
2025-04-27 21:19:14 -0300

    fix: adjust venda controller

--- src/main/java/com/br/demo/controller/VendaController.java
+++ src/main/java/com/br/demo/controller/VendaController.java
@@ -2,10 +2,12 @@ package com.br.demo.controller;
 
 import com.br.demo.dto.request.VendaRequestDTO;
 import com.br.demo.dto.response.VendaResponseDTO;
+import com.br.demo.model.Usuario;
 import com.br.demo.model.Venda;
 import com.br.demo.repository.VendaRepository;
 import com.br.demo.service.VendaService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,8 +26,8 @@ public class VendaController {
 	@PostMapping
 	@Operation(summary = "Create venda")
 	
-	public VendaResponseDTO createVenda(@RequestBody VendaRequestDTO vendaRequestDTO){
-		return vendaService.createVenda(vendaRequestDTO);
+	public VendaResponseDTO createVenda(@RequestBody VendaRequestDTO vendaRequestDTO, @AuthenticationPrincipal Usuario usuario){
+		return vendaService.createVenda(vendaRequestDTO, usuario);
 	}
 	
 	@GetMapping("/{id}")
