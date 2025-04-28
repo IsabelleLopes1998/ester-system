9eece338ce6da41c9640ac3598a3486c734e700f
Wicar Pessoa <wicarpp@gmail.com>
2025-04-28 15:31:02 -0300

    feat: created findPrecoVingenteByProdutoAhdData on historicoValorRepository

--- src/main/java/com/br/demo/repository/HistoricoValorRepository.java
+++ src/main/java/com/br/demo/repository/HistoricoValorRepository.java
@@ -1,8 +1,23 @@
 package com.br.demo.repository;
 
 import com.br.demo.model.HistoricoValor;
+import com.br.demo.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface HistoricoValorRepository extends JpaRepository<HistoricoValor, UUID> {}
+public interface HistoricoValorRepository extends JpaRepository<HistoricoValor, UUID> {
+    @Query(value = "SELECT * FROM historico_valor h " +
+            "WHERE h.id_produto = :produtoId " +
+            "AND h.data <= :data " +
+            "ORDER BY h.data DESC " +
+            "LIMIT 1", nativeQuery = true)
+    Optional<HistoricoValor> findPrecoVigenteByProdutoAndData(
+            @Param("produtoId") UUID produtoId,
+            @Param("data") LocalDate data
+    );
+}
