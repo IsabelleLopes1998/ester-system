182b3cd354156d8801c3936a9bc89e6cf43b1609
Wicar Pessoa <wicarpp@gmail.com>
2025-04-28 15:27:11 -0300

    feat: added authenticantion principal to compra controller

--- src/main/java/com/br/demo/controller/CompraController.java
+++ src/main/java/com/br/demo/controller/CompraController.java
@@ -2,8 +2,10 @@ package com.br.demo.controller;
 
 import com.br.demo.dto.request.CompraRequestDTO;
 import com.br.demo.dto.response.CompraResponseDTO;
+import com.br.demo.model.Usuario;
 import com.br.demo.service.CompraService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,8 +21,8 @@ public class CompraController {
     }
 
     @PostMapping
-    public ResponseEntity<CompraResponseDTO> criarCompra(@RequestBody CompraRequestDTO compraRequestDTO) {
-        CompraResponseDTO compra = compraService.criarCompra(compraRequestDTO);
+    public ResponseEntity<CompraResponseDTO> criarCompra(@RequestBody CompraRequestDTO compraRequestDTO, @AuthenticationPrincipal Usuario usuario) {
+        CompraResponseDTO compra = compraService.criarCompra(compraRequestDTO, usuario);
         return ResponseEntity.ok(compra);
     }
 
