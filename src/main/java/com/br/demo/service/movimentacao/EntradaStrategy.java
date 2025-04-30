54a55afb92e09acdccb14becfa08e2737fc87788
Wicar Pessoa <wicarpp@gmail.com>
2025-04-30 11:13:04 -0300

    fix: Entrada and Saida logic was inverted

--- src/main/java/com/br/demo/service/movimentacao/EntradaStrategy.java
+++ src/main/java/com/br/demo/service/movimentacao/EntradaStrategy.java
@@ -5,10 +5,7 @@ import com.br.demo.service.movimentacao.MovimentacaoStrategy;
 
 public class EntradaStrategy implements MovimentacaoStrategy {
     public void atualizarEstoque(Produto produto, int quantidade) {
-        if (produto.getQuantidadeEstoque() < quantidade) {
-            throw new IllegalArgumentException("Estoque insuficiente.");
-        }
-        produto.setQuantidadeEstoque(produto.getQuantidadeEstoque() - quantidade);
+        produto.setQuantidadeEstoque(produto.getQuantidadeEstoque() + quantidade);
     }
 }
 
--- src/main/java/com/br/demo/service/movimentacao/SaidaStrategy.java
+++ src/main/java/com/br/demo/service/movimentacao/SaidaStrategy.java
@@ -3,8 +3,12 @@ package com.br.demo.service.movimentacao;
 import com.br.demo.model.Produto;
 
 public class SaidaStrategy implements MovimentacaoStrategy {
+
     public void atualizarEstoque(Produto produto, int quantidade) {
-        produto.setQuantidadeEstoque(produto.getQuantidadeEstoque() + quantidade);
+        if (produto.getQuantidadeEstoque() < quantidade) {
+            throw new IllegalArgumentException("Estoque insuficiente.");
+        }
+        produto.setQuantidadeEstoque(produto.getQuantidadeEstoque() - quantidade);
     }
 }
 
