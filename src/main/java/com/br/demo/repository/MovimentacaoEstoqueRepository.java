bfa4aca4eb9409886baede4b7e71808d023a889a
Wicar Pessoa <wicarpp@gmail.com>
2025-04-30 14:35:43 -0300

    feat: create listar movimentcoes estoque por venda

--- src/main/java/com/br/demo/repository/MovimentacaoEstoqueRepository.java
+++ src/main/java/com/br/demo/repository/MovimentacaoEstoqueRepository.java
@@ -1,8 +1,11 @@
 package com.br.demo.repository;
 
+import com.br.demo.model.MovimentacaoEstoque;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface MovimentacaoEstoqueRepository extends JpaRepository<com.br.demo.model.MovimentacaoEstoque, UUID> {
+    List<MovimentacaoEstoque> findByVendaId(UUID vendaId);
 }
