3a7ac2bcbc8650d7b09c4f6db27c6b13a172e11c
Wicar Pessoa <wicarpp@gmail.com>
2025-05-01 13:58:41 -0300

    refactor: changed criarPagamento to simulate a asyncronous payment

--- src/main/java/com/br/demo/service/PagamentoService.java
+++ src/main/java/com/br/demo/service/PagamentoService.java
@@ -2,10 +2,12 @@ package com.br.demo.service;
 
 import com.br.demo.dto.request.PagamentoRequestDTO;
 import com.br.demo.dto.response.PagamentoResponseDTO;
+import com.br.demo.enums.StatusPagamento;
 import com.br.demo.model.Pagamento;
 import com.br.demo.repository.PagamentoRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -21,16 +23,30 @@ public class PagamentoService {
     }
 
     public PagamentoResponseDTO criarPagamento(PagamentoRequestDTO dto) {
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Erro durante simulação de atraso no pagamento", e);
+        }
+
+        StatusPagamento status = StatusPagamento.APROVADO;
+
         Pagamento pagamento = Pagamento.builder()
                 .formaPagamento(dto.getFormaPagamento())
+                .statusPagamento(status)
+                .valorTotal(dto.getValorTotal())
+                .data(LocalDate.now())
                 .build();
-
-        pagamento = pagamentoRepository.save(pagamento);
+        pagamentoRepository.save(pagamento);
 
         return PagamentoResponseDTO.builder()
                 .id(pagamento.getId())
                 .formaPagamento(pagamento.getFormaPagamento())
+                .statusPagamento(pagamento.getStatusPagamento())
+                .valorTotal(pagamento.getValorTotal())
                 .build();
+
     }
 
     public List<PagamentoResponseDTO> listarPagamentos() {
