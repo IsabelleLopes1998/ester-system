a84a8a784422fb25ba0eb9c916719af27abbb926
Wicar Pessoa <wicarpp@gmail.com>
2025-05-01 13:54:49 -0300

    feat: now venda calls pagamentoService

--- src/main/java/com/br/demo/service/VendaService.java
+++ src/main/java/com/br/demo/service/VendaService.java
@@ -2,12 +2,19 @@ package com.br.demo.service;
 
 import com.br.demo.dto.VendaItemDTO;
 import com.br.demo.dto.request.MovimentacaoEstoqueRequestDTO;
+import com.br.demo.dto.request.PagamentoRequestDTO;
 import com.br.demo.dto.request.VendaRequestDTO;
+import com.br.demo.dto.response.PagamentoResponseDTO;
+import com.br.demo.dto.response.VendaItemResponseDTO;
 import com.br.demo.dto.response.VendaResponseDTO;
+import com.br.demo.enums.StatusPagamento;
+import com.br.demo.enums.StatusVenda;
+import com.br.demo.enums.StatusVendaItem;
 import com.br.demo.enums.TipoMovimentacao;
 import com.br.demo.model.*;
 import com.br.demo.repository.*;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -16,158 +23,158 @@ import java.util.stream.Collectors;
 
 @Service
 public class VendaService {
-	
-	private final VendaRepository vendaRepository;
-	private final PagamentoRepository pagamentoRepository;
-	private final ClienteRepository clienteRepository;
-	private final ProdutoRepository produtoRepository;
-	private final VendaItemRepository vendaItemRepository;
-	private final HistoricoValorRepository historicoValorRepository;
-	private final MovimentacaoEstoqueService movimentacaoEstoqueService;
-	private final MovimentacaoEstoqueRepository movimentacaoEstoqueRepository;
 
-	public VendaService(
-			VendaRepository vendaRepository,
-			VendaItemRepository vendaItemRepository,
-			PagamentoRepository pagamentoRepository ,
-			ClienteRepository clienteRepository,
-			ProdutoRepository produtoRepository,
-			HistoricoValorRepository historicoValorRepository,
-			MovimentacaoEstoqueService movimentacaoEstoqueService,
-			MovimentacaoEstoqueRepository movimentacaoEstoqueRepository
-	) {
-		this.vendaRepository = vendaRepository;
-		this.pagamentoRepository = pagamentoRepository;
-		this.clienteRepository = clienteRepository;
-		this.produtoRepository = produtoRepository;
-		this.vendaItemRepository = vendaItemRepository;
-		this.historicoValorRepository = historicoValorRepository;
-		this.movimentacaoEstoqueService = movimentacaoEstoqueService;
-		this.movimentacaoEstoqueRepository = movimentacaoEstoqueRepository;
-	}
-	
-	public List<VendaResponseDTO> getAllVendas() {
-		return vendaRepository.findAll().stream()
-					   .map(this::toResponseDTO)
-					   .collect(Collectors.toList());
-	}
-	
-	public VendaResponseDTO getById(UUID id) {
-		return vendaRepository.findById(id)
-					   .map(this::toResponseDTO)
-					   .orElse(null);
-	}
-	//TODO: Temos que por a logica de pagamento aqui.
+	@Autowired
+	private VendaRepository vendaRepository;
+	@Autowired
+	private VendaItemService vendaItemService;
+	@Autowired
+	private PagamentoService pagamentoService;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	@Autowired
+	private  MovimentacaoEstoqueService movimentacaoEstoqueService;
+	@Autowired
+	private MovimentacaoEstoqueRepository movimentacaoEstoqueRepository;
+	@Autowired
+	private VendaItemRepository vendaItemRepository;
+
+		public List<VendaResponseDTO> getAllVendas () {
+			return vendaRepository.findAll().stream()
+					.map(this::toResponseDTO)
+					.collect(Collectors.toList());
+		}
+
+		public VendaResponseDTO getById (UUID id){
+			return vendaRepository.findById(id)
+					.map(this::toResponseDTO)
+					.orElse(null);
+		}
 	@Transactional
 	public VendaResponseDTO createVenda(VendaRequestDTO dto, Usuario usuario) {
 		Cliente cliente = clienteRepository.findById(dto.getIdCliente())
-		.orElseThrow(() -> new IllegalArgumentException("Cliente não encontrado: " + dto.getIdCliente()));
-		Pagamento pagamento = pagamentoRepository.findById(dto.getIdPagamento()).orElse(null);
-
-		if (usuario == null || cliente == null || pagamento == null) {
-			throw new IllegalArgumentException("Usuário, cliente ou pagamento não encontrados.");
-		}
-
-		for (VendaItemDTO vendaItem : dto.getVendaItemList()) {
-			Produto produto = produtoRepository.findById(vendaItem.getProdutoId())
-					.orElseThrow(() -> new IllegalArgumentException("Produto não encontrado: " + vendaItem.getProdutoId()));
-
-			if (produto.getQuantidadeEstoque() < vendaItem.getQuantidadeVenda()) {
-				throw new IllegalArgumentException("Estoque insuficiente para o produto: " + produto.getNome());
-			}
-		}
+				.orElseThrow(() -> new IllegalArgumentException("Cliente não encontrado: " + dto.getIdCliente()));
 
 		Venda venda = Venda.builder()
 				.data(dto.getData())
 				.usuario(usuario)
 				.cliente(cliente)
-				.pagamento(pagamento)
+				.statusVenda(StatusVenda.AGUARDANDO_PAGAMENTO)
 				.build();
+		venda = vendaRepository.save(venda);
+
+		List<VendaItem> vendaItens = vendaItemService.criarItensVenda(venda, dto.getVendaItemList(), dto.getData());
+
+		venda.setVendaItens(vendaItens);
+
+		PagamentoRequestDTO pagamentoRequestDTO = new PagamentoRequestDTO();
+		pagamentoRequestDTO.setFormaPagamento(dto.getFormaPagamento());
+		pagamentoRequestDTO.setStatusPagamento(StatusPagamento.PENDENTE);
+		pagamentoRequestDTO.setValorTotal(venda.getValorTotal());
+
+		PagamentoResponseDTO pagamentoResponseDTO = pagamentoService.criarPagamento(pagamentoRequestDTO);
+
+		Pagamento pagamento = Pagamento.builder()
+				.id(pagamentoResponseDTO.getId())
+				.formaPagamento(pagamentoRequestDTO.getFormaPagamento())
+				.statusPagamento(pagamentoResponseDTO.getStatusPagamento())
+				.valorTotal(pagamentoRequestDTO.getValorTotal())
+				.build();
+		venda.setPagamento(pagamento);
 
 		venda = vendaRepository.save(venda);
 
-		List<VendaItem> vendaItens = new ArrayList<>();
+		if (pagamentoResponseDTO.getStatusPagamento() == StatusPagamento.APROVADO) {
+			confirmarVenda(venda, usuario);
+		} else if (pagamentoResponseDTO.getStatusPagamento() == StatusPagamento.RECUSADO) {
+			cancelarVenda(venda);
+		}
 
-		for (VendaItemDTO vendaItemDTO : dto.getVendaItemList()) {
-			Produto produto = produtoRepository.findById(vendaItemDTO.getProdutoId())
-					.orElseThrow(() -> new IllegalArgumentException("Produto não encontrado: " + vendaItemDTO.getProdutoId()));
+		return toResponseDTO(venda);
+	}
 
-			MovimentacaoEstoqueRequestDTO novaMovimentacao = new MovimentacaoEstoqueRequestDTO(
-					vendaItemDTO.getProdutoId(),
-					dto.getData(),
-					vendaItemDTO.getQuantidadeVenda(),
+	@Transactional
+		public VendaResponseDTO updateVenda (UUID id, VendaRequestDTO dto){
+			Venda venda = vendaRepository.findById(id)
+					.orElseThrow(() -> new IllegalArgumentException("Venda não encontrada: " + id));
+
+			return toResponseDTO(venda);
+		}
+
+		@Transactional
+		public void deleteVenda (UUID id){
+			Venda venda = vendaRepository.findById(id)
+					.orElseThrow(() -> new IllegalArgumentException("Venda não encontrada: " + id));
+
+			for (VendaItem item : venda.getVendaItens()) {
+				Produto produto = item.getProduto();
+				produto.setQuantidadeEstoque(produto.getQuantidadeEstoque() + item.getQuantidade());
+				produtoRepository.save(produto);
+			}
+
+			List<MovimentacaoEstoque> movimentacoes = movimentacaoEstoqueRepository.findByVendaId(venda.getId());
+			movimentacaoEstoqueRepository.deleteAll(movimentacoes);
+
+			vendaRepository.delete(venda);
+		}
+
+		private VendaResponseDTO toResponseDTO (Venda venda){
+			return VendaResponseDTO.builder()
+					.id(venda.getId())
+					.usernameUsuario(venda.getUsuario().getUsername())
+					.idCliente(venda.getCliente().getId())
+					.idPagamento(venda.getPagamento().getId())
+					.dataVenda(venda.getData())
+					.vendaItemList(
+							venda.getVendaItens() != null ?
+									venda.getVendaItens().stream()
+											.map(vendaItem -> VendaItemDTO.builder()
+													.produtoId(vendaItem.getProduto().getId())
+													.quantidadeVenda(vendaItem.getQuantidade())
+													.build())
+											.collect(Collectors.toList())
+									: null
+					)
+					.valorTotal(venda.getValorTotal())
+					.statusVenda(venda.getStatusVenda())
+					.build();
+		}
+
+	private void confirmarVenda(Venda venda, Usuario usuario) {
+		for (VendaItem item : venda.getVendaItens()) {
+			item.setStatusVendaItem(StatusVendaItem.CONFIRMADO);
+
+			MovimentacaoEstoqueRequestDTO movimentacao = new MovimentacaoEstoqueRequestDTO(
+					item.getProduto().getId(),
+					venda.getData(),
+					item.getQuantidade(),
 					"VENDA",
 					"SAIDA",
 					null,
 					venda.getId()
 			);
 
-			movimentacaoEstoqueService.criar(novaMovimentacao, usuario);
-			produtoRepository.save(produto);
-
-			HistoricoValor precoVigente = historicoValorRepository.findPrecoVigenteByProdutoAndData(
-					produto.getId(), dto.getData()
-			).orElseThrow(() -> new IllegalArgumentException("Preço não encontrado para produto " + produto.getId()));
-
-			VendaItem vendaItem = VendaItem.builder()
-					.id(new VendaItemId(venda.getId(), produto.getId()))
-					.venda(venda)
-					.produto(produto)
-					.quantidade(vendaItemDTO.getQuantidadeVenda())
-					.preçoUnitario(precoVigente.getPreçoUnitario())
-					.build();
-
-			vendaItens.add(vendaItem);
+			movimentacaoEstoqueService.criar(movimentacao, usuario);
 		}
 
-		venda.setVendaItens(vendaItens);
+		vendaItemRepository.saveAll(venda.getVendaItens());
 
-		return toResponseDTO(venda);
-	}
-	//TODO: se quiser adicionar o delete logico para ficar tudo registrado, seria aqui.
-	@Transactional
-	public VendaResponseDTO updateVenda(UUID id, VendaRequestDTO dto) {
-		Venda venda = vendaRepository.findById(id)
-				.orElseThrow(() -> new IllegalArgumentException("Venda não encontrada: " + id));
+		venda.setStatusVenda(StatusVenda.CONFIRMADO);
 
-		return toResponseDTO(venda);
+		vendaRepository.save(venda);
 	}
 
-	@Transactional
-	public void deleteVenda(UUID id) {
-		Venda venda = vendaRepository.findById(id)
-				.orElseThrow(() -> new IllegalArgumentException("Venda não encontrada: " + id));
 
+	private void cancelarVenda(Venda venda) {
 		for (VendaItem item : venda.getVendaItens()) {
-			Produto produto = item.getProduto();
-			produto.setQuantidadeEstoque(produto.getQuantidadeEstoque() + item.getQuantidade());
-			produtoRepository.save(produto);
+			item.setStatusVendaItem(StatusVendaItem.CANCELADO);
 		}
 
-		List<MovimentacaoEstoque> movimentacoes = movimentacaoEstoqueRepository.findByVendaId(venda.getId());
-		movimentacaoEstoqueRepository.deleteAll(movimentacoes);
+		vendaItemRepository.saveAll(venda.getVendaItens());
 
-		vendaRepository.delete(venda);
+		venda.setStatusVenda(StatusVenda.CANCELADO);
+		vendaRepository.save(venda);
 	}
-
-	private VendaResponseDTO toResponseDTO(Venda venda) {
-		return VendaResponseDTO.builder()
-				.id(venda.getId())
-				.usernameUsuario(venda.getUsuario().getUsername())
-				.idCliente(venda.getCliente().getId())
-				.idPagamento(venda.getPagamento().getId())
-				.dataVenda(venda.getData())
-				.vendaItemList(
-						venda.getVendaItens() != null ?
-								venda.getVendaItens().stream()
-										.map(vendaItem -> VendaItemDTO.builder()
-												.produtoId(vendaItem.getProduto().getId())
-												.quantidadeVenda(vendaItem.getQuantidade())
-												.build())
-										.collect(Collectors.toList())
-								: null
-				)
-				.valorTotal(venda.getValorTotal())
-				.build();
 	}
-}
