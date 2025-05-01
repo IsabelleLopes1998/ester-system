60efa91cf15bb9a267038e64e11ec554717b386c
Wicar Pessoa <wicarpp@gmail.com>
2025-05-01 13:59:37 -0300

    feat: added jakarta validations to compraItem

--- src/main/java/com/br/demo/model/CompraItem.java
+++ src/main/java/com/br/demo/model/CompraItem.java
@@ -1,6 +1,8 @@
 package com.br.demo.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -20,29 +22,33 @@ public class CompraItem {
     @EmbeddedId
     private CompraItemId id;
 
+    @NotNull(message = "Produto é obrigatório.")
     @ManyToOne
     @MapsId("produtoId")
     @JoinColumn(name = "id_produto", nullable = false)
     private Produto produto;
 
+    @NotNull(message = "Compra é obrigatória.")
     @ManyToOne
     @MapsId("compraId")
     @JoinColumn(name = "id_compra", nullable = false)
     private Compra compra;
 
+    @NotNull(message = "Quantidade é obrigatória.")
+    @Min(value = 1, message = "Quantidade deve ser maior que zero.")
     @Column(nullable = false)
-    @NotNull(message = "Quantidade é obrigatório.")
     private Integer quantidade;
 
+    @NotNull(message = "Preço unitário é obrigatório.")
+    @DecimalMin(value = "0.01", inclusive = true, message = "O valor não pode ser menor que 0.01.")
     @Column(nullable = false)
     private BigDecimal preçoUnitario;
 
-    @Column(name = "created_at", nullable = false, updatable = false)
     @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @Column(nullable = false)
     @UpdateTimestamp
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
-
 }
